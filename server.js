e6246e55ab52f605a18538a342eeb91eba723239
lllMISHRAlll <soumyaranjanmishra29@gmail.com>
2025-05-09 20:26:25 +0530

    default route added

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use("/", (req, res) => {
+  res.send({
+    activeStatus: true,
+  });
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/tickets", ticketRoutes);
 app.use("/api/user", userRoutes);
