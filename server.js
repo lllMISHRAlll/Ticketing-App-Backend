57f016970d94f150666961140fc166d345c2abca
lllMISHRAlll <soumyaranjanmishra29@gmail.com>
2025-05-12 17:47:05 +0530

    allowed all origins request

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
